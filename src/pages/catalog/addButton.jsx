0122c54ea8c52105a13bc94eb4d3aa361617a599
Your Name <you@example.com>
2024-07-25 01:01:28 +0300

    1

--- src/pages/catalog/addButton.jsx
+++ src/pages/catalog/addButton.jsx
@@ -1,7 +1,7 @@
-import {   Button  , IconButton ,ButtonGroup }
+import {   Button  , IconButton ,ButtonGroup,CircularProgress }
 from '@mui/material';
 import kdb from "../../kadabrix/kadabrix";
-import { useContext } from 'react';
+import { useState , useEffect  , useContext } from 'react';
 import { CartContext } from '../../kadabrix/cartState';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
@@ -11,14 +11,22 @@ const AddButton = (props) => {
 
 const { cart , setCart } = useContext(CartContext);
 
+const [ isLoading , setIsLoading ] = useState(false);
+
+
 const inCart = (partName) => {
     return (cart.items.find((item)=>item.partName==partName))
 }
     
 
+useEffect(()=>{
+
+    setIsLoading(false)
+
+},[cart])
 
 const removeProduct = async (partName) => {
-
+    setIsLoading(true)
 // find index in cart 
     let item = inCart(partName)
     let index = item.index;
@@ -33,7 +41,7 @@ const removeProduct = async (partName) => {
 
    
  const addProduct = async (item) => {
-
+    setIsLoading(true)
     let data = await kdb.run({
       "module": "kdb_cart",
       "name": "addItem",
@@ -51,7 +59,20 @@ const removeProduct = async (partName) => {
    
     
     return (
+        <div style={{ m: 1, position: 'relative' }}>{isLoading?
+            <CircularProgress
+            size={68}
+            sx={{
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              zIndex: 1,
+            }}
+          />
+
+        :''}
         <ButtonGroup variant="contained" aria-label="Basic button group">
+        
         {(inCart(props.item.partName))? 
         <div>
                         
@@ -71,7 +92,11 @@ const removeProduct = async (partName) => {
         </IconButton>
 
         }
-        </ButtonGroup>)
+        </ButtonGroup>
+        </div>
+        )
+        
+
 
 }
 export default AddButton
