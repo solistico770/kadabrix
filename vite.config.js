022cc6a53e61467d441170ce66f4f62aff1d775a
Your Name <you@example.com>
2024-08-08 00:24:50 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createRoutesPlugin } from './scripts/vite-plugin-routes';
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"kadabrix",
+    short_name:"kadabrix",
+    description:"kadabrix",
+    icons:[
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA(manifestForPlugIn),
     createRoutesPlugin({
       pagesDir: 'src/pages',
       output: 'src/routes.jsx'
