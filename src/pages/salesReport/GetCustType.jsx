bee506ecb564936cc2c9f2ee14e1bd7484efcf77
Your Name <you@example.com>
2024-08-26 15:17:11 +0300

    1

---
+++ src/pages/salesReport/GetCustType.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import kdb from '../../kadabrix/kadabrix';
+import { Autocomplete, TextField, CircularProgress } from '@mui/material';
+
+const Data = (props) => {
+  
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [debouncedValue, setDebouncedValue] = useState(inputValue);
+  const [loading, setLoading] = useState(false);
+
+  
+
+  const fetchData = async (searchTerm = '') => {
+    setLoading(true);
+    try {
+      const response = await kdb.run({
+        module: 'salesReport',
+        name: 'getCustType',
+        data: { search: searchTerm },
+      });
+      setData(response);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Debounce effect
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(inputValue);
+    }, 500); // 500ms debounce delay
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [inputValue]);
+
+  // Fetch data when debounced value changes
+  useEffect(() => {
+    if (debouncedValue) {
+      fetchData(debouncedValue);
+    } else {
+      setData([]); // Clear data if input is empty
+    }
+  }, [debouncedValue]);
+
+
+  useEffect(() => {
+    
+    fetchData();
+
+  }, []);
+
+  return (
+    <div>
+    <Autocomplete
+      options={data}
+      onChange={(event, newValue) => {
+        props.setter(newValue); // Update selected item state
+      }}
+      getOptionLabel={(option) => `${option.cType} ${option.cTypeDes || ''}`}
+      filterOptions={(x) => x} // Disable built-in filtering to rely on server-side filtering
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="בחר סוג לקוח"
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+      style={{ width: 300 }}
+    />
+    </div>
+  );
+};
+
+export default Data;
