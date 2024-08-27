e950cb4131fb2066bdcb7eb4b642923a3ae24f9a
Your Name <you@example.com>
2024-08-27 18:58:06 +0300

    1

---
+++ src/pages/salesReport/GetDateGroup.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Grid from '@mui/material/Grid';
+
+const Data = (props) => {
+  
+  
+
+  const handleChange = (data) => {
+    props.setter(data);
+  };
+
+  return (
+   
+
+        <Grid container spacing={2} sx={{ marginTop: 2 }}>
+          <Grid item xs={12}>
+            <ButtonGroup variant="outlined" fullWidth aria-label="Basic button group">
+              <Button onClick={()=>{handleChange("day")}} >יום</Button>
+              <Button onClick={()=>{handleChange("week")}} >שבוע</Button>
+              <Button onClick={()=>{handleChange("month")}} >חודש</Button>
+              <Button onClick={()=>{handleChange("year")}} >שנה</Button>
+              <Button onClick={()=>{handleChange("none")}} >ללא</Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+ 
+  );
+};
+
+export default Data;
