6906fad4aa0048c848bef70f7e48709caf92d42c
Your Name <you@example.com>
2024-09-24 18:02:29 +0300

    1

---
+++ src/pages/codeEditor/index.jsx
@@ -0,0 +1,293 @@
+import React, { useState, useEffect } from 'react';
+import { Button, TextField, Grid, Box, Typography, Alert, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import kdb from '../../kadabrix/kadabrix';
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+import Modal from '@mui/material/Modal';
+
+
+const Users = () => {
+  const [error, setError] = useState(null);
+  
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+  const [editorItem, setEditorItem] = useState({});
+  const [editorText, setEditorText] = useState("");
+
+  const [lines, setLines] = useState([]);
+  const [linesSearch, setLinesSearch] = useState([]); 
+  const [searchType, setSearchType] = useState();
+  const [searchName, setSearchName ]  = useState();
+  const [searchModule, setSearchModule] = useState();
+  const [searchAll, setSearchAll] = useState();
+
+  const searchInLines = (searchCriteria, searchAll, lines) => {
+    return lines.filter((line) => {
+      // If searchAll is true, check if any field in the line contains any of the search terms
+      if (searchAll) {
+        return Object.values(line).some((field) =>
+          Object.values(searchCriteria).some((criteria) =>
+            criteria ? field.includes(criteria) : false
+          )
+        );
+      }
+  
+      // Otherwise, search based on specific fields in searchCriteria
+      let match = true;
+  
+      // Loop over each key in searchCriteria and check for matching in the line object
+      Object.keys(searchCriteria).forEach((key) => {
+        const criteria = searchCriteria[key];
+        if (criteria && line[key] && !line[key].includes(criteria)) {
+          match = false;
+        }
+      });
+  
+      return match;
+    });
+  };
+
+  
+  useEffect(()=>{
+
+    let filtered  = searchInLines({
+        "type":searchType,
+        "name":searchName,
+        "module":searchModule,
+
+    },searchAll,lines)
+
+    setLinesSearch(filtered);
+
+    
+  }
+  ,[lines,searchType,searchName,searchModule,searchAll])
+
+
+  const fetchApp = async () => {
+    try {
+      const data = await kdb.run({
+        "module": "codeEditor",
+        "name": "getRecords"
+      });
+
+      setLines(data);
+    } catch (err) {
+      setError(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchApp();
+  }, []);
+
+  
+const handleisEditorOpen = () => {
+  setIsEditorOpen(false);
+}
+
+const openEditor = (record)=>{
+
+  setEditorItem(record);
+  setEditorText(record.data)
+  setIsEditorOpen(true);
+}
+
+const doSave = async (record)=>{
+
+  const data = await kdb.run({
+    "module": "codeEditor",
+    "name": "saveRecord",
+    "data":editorItem
+  });
+
+  fetchApp();
+  setIsEditorOpen(false);
+
+
+}
+
+const doDelete = async ()=>{
+  const data = await kdb.run({
+    "module": "codeEditor",
+    "name": "delRecord",
+    "data":{
+      id:editorItem.id
+    }
+  });
+}
+
+
+const addRecord = async (record)=>{
+  const data = await kdb.run({
+    "module": "codeEditor",
+    "name": "addRecord",
+    "data":{
+        type:searchType,
+        name:searchName,
+        module:searchModule,
+    }
+  });
+}
+
+
+
+
+
+  return (
+    <div dir="ltr">
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        mt: 8,
+        p: 3,
+        borderRadius: 2,
+        boxShadow: 3,
+        bgcolor: 'background.paper',
+      }}
+    >
+      <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+        Code Editor
+      </Typography>
+      {error && (
+        <Alert severity="error" sx={{ width: '100%', mb: 2 }}>
+          {error}
+        </Alert>
+      )}
+
+<Modal
+  open={isEditorOpen}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box
+    sx={{
+      backgroundColor: '#fff', // Desired background color
+      borderRadius: '8px',
+      boxShadow: '0 4px 30px rgba(0,0,0,0.1)',
+      padding: '16px',
+      width: '95%',
+      height: '95%', // Set the height to 95%
+      maxWidth: '95vw', // Ensure it doesn't exceed the viewport width
+      maxHeight: '95vh', // Ensure it doesn't exceed the viewport height
+      margin: 'auto',
+      overflow: 'auto', // Enable scrolling if content overflows
+      position: 'absolute', // Positioning for centering
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)', // Center the modal
+    }}
+  >
+    <Box>
+      <Button onClick={() => setIsEditorOpen(false)}>Close</Button>
+      <Button onClick={() => doSave()}>Save</Button>
+      <Button onClick={() => doDelete()}>Delete</Button>
+    </Box>
+    <Box>
+      <TextField
+        label="Type"
+        value={editorItem.type}
+        onChange={(e) => { setEditorItem({ ...editorItem, type: e.target.value }); }}
+      />
+      <TextField
+        label="Module"
+        value={editorItem.module}
+        onChange={(e) => { setEditorItem({ ...editorItem, module: e.target.value }); }}
+      />
+      <TextField
+        label="Name"
+        value={editorItem.name}
+        onChange={(e) => { setEditorItem({ ...editorItem, name: e.target.value }); }}
+      />
+      <TextField
+        label="Config"
+        value={editorItem.config}
+        onChange={(e) => { setEditorItem({ ...editorItem, config: e.target.value }); }}
+      />
+    </Box>
+    <Box>
+      <AceEditor
+        mode="java"
+        theme="github"
+        width="100%"
+        height={window.innerHeight+"px"} // Set a specific height for the editor
+        value={editorItem.data}
+        onChange={(newValue) => { setEditorItem({ ...editorItem, data: newValue }) }}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+          fontSize: '32px',
+        }}
+      />
+    </Box>
+  </Box>
+</Modal>
+
+
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+          
+          <TableRow>
+              <TableCell><TextField onChange={(e)=>{setSearchType(e.target.value)}}/></TableCell>
+              <TableCell><TextField onChange={(e)=>{setSearchModule(e.target.value)}}/></TableCell>
+              <TableCell><TextField onChange={(e)=>{setSearchName(e.target.value)}}/></TableCell>
+              <TableCell><TextField onChange={(e)=>{setSearchAll(e.target.value)}}/> </TableCell>
+              <TableCell>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        startIcon={<span>+</span>} 
+        onClick={() => addRecord()}
+        sx={{ mb: 2 }}
+      >
+        Add Row
+      </Button>
+</TableCell>
+        </TableRow>
+          
+
+
+            <TableRow>
+              <TableCell>סוג</TableCell>
+              <TableCell>מודול</TableCell>
+              <TableCell>שם</TableCell>
+              <TableCell>DI</TableCell>
+              <TableCell>קוד</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {linesSearch.map((line) => (
+              <TableRow key={line.email}>
+                
+                <TableCell>{line.type}</TableCell>
+                <TableCell>{line.module}</TableCell>
+                <TableCell>{line.name}</TableCell>
+                <TableCell>{line.description}</TableCell>
+                <TableCell><Button onClick={()=>{
+
+                    openEditor(line);
+
+                }}>ערוך</Button></TableCell>
+                </TableRow>
+            ))}
+          </TableBody>
+
+          
+        </Table>
+      </TableContainer>
+    </Box>
+    </div>
+  );
+};
+
+export default Users;
