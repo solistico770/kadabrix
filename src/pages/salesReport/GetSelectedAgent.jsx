30207d4ca57a1b97617a9a77d39bea8828aa576d
Your Name <shay@solisti.co>
2024-09-30 16:57:05 +0300

    1

---
+++ src/pages/salesReport/GetSelectedAgent.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import kdb from '../../kadabrix/kadabrix';
+import { Autocomplete, TextField, CircularProgress ,Button} from '@mui/material';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+
+
+const Data = (props) => {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  
+
+
+  return (
+    <div>
+        { (props.state!=null) ? 
+    (<div> 
+
+<Button 
+         startIcon={<HighlightOffIcon />}
+         onClick={()=>{
+            props.setter(null);
+         }}></Button>
+
+
+
+         <Button 
+         startIcon={<SupportAgentIcon />}
+         ></Button>
+
+        
+
+         {props.state.agentName} {props.state.agentDes }
+         
+
+    </div>)  : ''    }
+
+  </div>
+  );
+};
+
+export default Data;
---
+++ src/pages/salesReport/GetSelectedPart.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import kdb from '../../kadabrix/kadabrix';
+import { Autocomplete, TextField, CircularProgress ,Button} from '@mui/material';
+
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+const Data = (props) => {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  
+
+
+  return (
+    <div>
+        { (props.state!=null) ? 
+    (<div> 
+          <Button 
+         startIcon={<HighlightOffIcon />}
+         onClick={()=>{
+          props.setter(null);
+       }}
+       
+         ></Button>
+
+        <Button 
+         startIcon={<ShoppingCartIcon />}
+         ></Button>
+
+         {props.state.partName} {props.state.partDes }
+
+
+    </div>)  : ''    }
+
+  </div>
+  );
+};
+
+export default Data;
