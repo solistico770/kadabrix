22393b6987348a3e5564f0313a4decea23362b4e
Your Name <shay@solisti.co>
2024-11-20 20:13:19 +0200

    1

--- src/kadabrix/layout.jsx
+++ src/kadabrix/layout.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import kdb from './kadabrix';
-import CartComponent from "./cartComponent"
+import CartComponent from "./cartComponent";
+import logo from '../assets/logo.png';
 
 const Layout = () => {
   const [user, setUser] = useState(null);
@@ -34,33 +35,43 @@ const Layout = () => {
   return (
     <Box>
       <AppBar position="static">
-        <Toolbar>
-        <Typography variant="h6" >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          {/* Logo on the left */}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
           <Button color="inherit" onClick={() => navigate('/menu')}>
-        KaDaBriX
-          </Button>
-              
-          </Typography>
-
-          <Typography variant="h6" >
-          <Button color="inherit">
-          <CartComponent/>
-
-          </Button>
-              
-          </Typography>
-
-
-
-          {user ? (
-            <Button color="inherit" onClick={handleLogout}>
-              יציאה
+                
+                
+            <img 
+              src={logo} 
+              alt="Logo" 
+              style={{ width: 60, height: 'auto', marginRight: '16px' ,  mixBlendMode: 'multiply'
+              }} 
+            />
             </Button>
-          ) : (
-            <Button color="inherit" onClick={() => navigate('/login')}>
-              כניסה
-            </Button>
-          )}
+            
+          </Box>
+
+          {/* Center Cart Component */}
+          <Box>
+            <Typography variant="h6">
+              <Button color="inherit">
+                <CartComponent />
+              </Button>
+            </Typography>
+          </Box>
+
+          {/* Login/Logout Button on the right */}
+          <Box>
+            {user ? (
+              <Button color="inherit" onClick={handleLogout}>
+                יציאה
+              </Button>
+            ) : (
+              <Button color="inherit" onClick={() => navigate('/login')}>
+                כניסה
+              </Button>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
