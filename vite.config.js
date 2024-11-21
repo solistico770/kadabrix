3cf04f4ca02a9e866725a14b0c4b3b7d33843b3b
Your Name <shay@solisti.co>
2024-11-21 11:53:12 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,43 @@
-import { defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createRoutesPlugin } from './scripts/vite-plugin-routes';
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa';
 import { createfilesTreePlugin } from './scripts/vite-plugin-filesTree';
+
 const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"kadabrix",
-    short_name:"kadabrix",
-    description:"kadabrix",
-    icons:[
-  ],
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
-}
+  registerType: 'autoUpdate',
+  includeAssets: [],
+  manifest: {
+    name: 'kadabrix',
+    short_name: 'kadabrix',
+    description: 'kadabrix',
+    icons: [],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'js-css-cache',
+        },
+      },
+      {
+        urlPattern: ({ request }) => request.destination === 'document',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'html-cache',
+        },
+      },
+    ],
+  },
+};
 
 export default defineConfig({
   build: {
@@ -31,7 +49,7 @@ export default defineConfig({
     createfilesTreePlugin(),
     createRoutesPlugin({
       pagesDir: 'src/pages',
-      output: 'src/routes.jsx'
-    })
-  ]
+      output: 'src/routes.jsx',
+    }),
+  ],
 });
