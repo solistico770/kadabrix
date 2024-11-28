4766d3d3b304920e682f7e7d525ebbe9a3ff89a2
Your Name <shay@solisti.co>
2024-11-28 14:40:29 +0200

    1

--- src/pages/catalog/catalogCats.jsx
+++ src/pages/catalog/catalogCats.jsx
@@ -3,16 +3,13 @@ import kdb from '../../kadabrix/kadabrix';
 import { Typography, Card, CardContent, CardMedia, IconButton, Menu, MenuItem } from '@mui/material';
 import { supabaseUrl } from "../../kadabrix/kdbConfig";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
 
 const SubCats = (props) => {
-
-
-
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
 
@@ -55,7 +52,6 @@ const SubCats = (props) => {
 };
 
 const CatalogCats = (props) => {
-
     const [winw, setWinw] = useState(window.innerWidth);
     useEffect(() => {
         const handleResize = () => {
@@ -67,19 +63,14 @@ const CatalogCats = (props) => {
         };
     }, []);
 
-
     const viewSize = (ranges, size) => {
-        // Iterate through each range in the array
         for (let range of ranges) {
-            // Check if the given size falls within the current range
             if (size >= range.from && size <= range.to) {
                 return range.size;
             }
         }
-        // Optional: If no range is matched, return a default value (like 0 or null)
-        return null; // or you could return some default value
-    }
-
+        return null;
+    };
 
     const [cats, setCats] = useState([]);
     const childrenOf = (father) => {
@@ -101,111 +92,53 @@ const CatalogCats = (props) => {
         fetchCats();
     }, []);
 
-
-
-    const settings = {
-        autoplay: true,
-        pauseOnHover: true,
-        autoplaySpeed: 4000,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 9, // Default number of slides to show
-        slidesToScroll: 5, // Default number of slides to scroll
-        initialSlide: 3,
-        responsive: [
-
-            {
-                breakpoint: 1200, // For large tablets and desktops
-                settings: {
-                    slidesToShow: 8,
-                    slidesToScroll: 5,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 1024, // For tablets in landscape
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 4,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 768, // For tablets in portrait mode
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 600, // For larger mobile devices
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                    initialSlide: 1,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-            {
-                breakpoint: 480, // For smaller mobile devices
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 4,
-                    infinite: true,
-                    dots: true,
-                },
-            },
-        ],
-    };
-
-
     const cardHeight = viewSize([
-        { from: 0, to: 480, size: 60 },
-        { from: 481, to: 99999, size: 118 },
+        { from: 0, to: 600, size: 60 },
+        { from: 600, to: 99999, size: 118 },
     ], winw);
 
     const cardWidth = viewSize([
-        { from: 0, to: 480, size: 140 },
-        { from: 481, to: 99999, size: 190 },
+        { from: 0, to: 600, size: 140 },
+        { from: 600, to: 99999, size: 190 },
     ], winw);
 
-
     return (
-        
-        <div className="slider-container">
-        {cardHeight}
-            <Slider {...settings}>
-
-
+            <Swiper
+                navigation={false}
+                modules={[Navigation, Pagination]}
+                spaceBetween={10}
+                slidesPerView={9}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    1200: { slidesPerView: 8 },
+                    1024: { slidesPerView: 6 },
+                    768: { slidesPerView: 4 },
+                    600: { slidesPerView: 3 },
+                    480: { slidesPerView: 6 },
+                    1: { slidesPerView: 6   },
+                }}
+            >
                 {cats.filter((cat) => cat.father === 0).map((category, index) => (
-                    <div>
+                    <SwiperSlide key={index}>
                         <Card
-                            key={index}
                             onClick={() => props.setCat(category.id)}
                             sx={{
                                 width: cardHeight,
                                 height: cardWidth,
                                 cursor: 'pointer',
-                                transition: 'transform 0.3s ease-in-out', // Smooth transition
+                                transition: 'transform 0.3s ease-in-out',
                                 '&:hover': {
-                                    transform: 'scale(1.05)', // Grow slightly on hover
+                                    transform: 'scale(1.05)',
                                 },
                             }}
                         >
                             <CardMedia
                                 component="img"
-                                height="{cardHeight}"
+                                height={cardHeight}
                                 image={`${supabaseUrl}/storage/v1/render/image/public/cats/${category.id}.jpg?width=118&height=118`}
                                 alt={category.name}
                             />
                             <CardContent style={{ padding: '4px' }}>
-
                                 <Typography
                                     variant="subtitle2"
                                     component="div"
@@ -220,15 +153,9 @@ const CatalogCats = (props) => {
                                 <SubCats setCat={props.setCat} children={childrenOf(category.id)} />
                             </CardContent>
                         </Card>
-                    </div>
-
-
+                    </SwiperSlide>
                 ))}
-
-
-            </Slider>
-        </div>
-
+            </Swiper>
     );
 };
 
