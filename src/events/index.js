0509a891205c4bff9bba1bcbc53804bd5e3570e1
Your Name <shay@solisti.co>
2024-12-04 13:08:14 +0200

    1

---
+++ src/events/index.js
@@ -0,0 +1,12 @@
+const runListeners = async () => {
+    const eventModules = import.meta.glob('./listeners/*.js'); // Adjust the path if necessary
+  
+    for (const path in eventModules) {
+      const module = await eventModules[path]();
+      if (typeof module.default === 'function') {
+        module.default(); // Execute the default exported function
+      }
+    }
+  };
+  
+  export default runListeners
