e0b3ee6b1aedb77a1dcf9d1b96b20cb2fc757fe8
Your Name <shay@solisti.co>
2025-01-26 15:21:12 +0200

    1

--- src/events/listeners/postLoginNavigate.jsx
+++ src/events/listeners/postLoginNavigate.jsx
@@ -1,12 +1,13 @@
 import eventBus from "../../kadabrix/event";
 import {useUserStore} from "../../kadabrix/userState";
-import {useCartStore } from "../../kadabrix/cartState";
+
 
 
 eventBus.on("postAuthStateChange", (payload) => {
   const userState = useUserStore.getState().userDetails
-  if (window.location.pathname=='/login') {
+  if (window.location.pathname=='' ||window.location.pathname=='/login') {
   eventBus.emit("navigate", userState.config.defaultScreen);
   }
+  
   
 });
