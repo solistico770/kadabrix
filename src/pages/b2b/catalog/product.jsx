9e1d0d17c7dd8861793467773415680f26a6d6d1
Your Name <shay@solisti.co>
2025-01-26 15:56:23 +0200

    1

--- src/pages/b2b/catalog/product.jsx
+++ src/pages/b2b/catalog/product.jsx
@@ -1,40 +1,19 @@
+import React, { useState } from 'react';
 import AddButton from "./addButton.jsx";
-import DetailsButton from "./detailsButton.jsx";
 import imageOnError from '../../../kadabrix/imgErr.js';
 import { FaEye } from "react-icons/fa";
 import { supabaseUrl } from "../../../kadabrix/kdbConfig";
 import "./product.css";
+import DetailsButton from './detailsButton.jsx';
 
-const Product = ({ products }) => {
-  const currencyFormat = (num) => {
-    num = Number(num);
-    return '₪' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-  };
+const Product = ({ product }) => {
 
-  return (
-    // Main grid container
-    <div className="productbox grid 
-                xxl:text-xl
-                2xl:text-xl
-                xl:text-xl 
-                lg:text-lg 
-                slg:text-base 
-                md:text-base 
-                sm:text-sm 
-                xsm:text-sm  
-                xxsm:text-lg
-                xxl:grid-cols-5
-                2xl:grid-cols-5
-                xl:grid-cols-4
-                lg:grid-cols-4
-                slg:grid-cols-3
-                md:grid-cols-3
-                sm:grid-cols-2 
-                xsm:grid-cols-2 
-                xxsm:grid-cols-1
-                gap-6
-                p-6">
-      {products.map((product) => (
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  return (  
+
+
+    
         // Product card container with smooth transitions
         <div  key={product.partName} className="bg-white rounded-xl shadow-xl hover:shadow-xl transition-all duration-300 transform hover:scale-105 hover:translate-y-1 hover:shadow-lg 
                         border border-gray-100 hover:border-primary/30 h-full flex flex-col max-w-[300px] 
@@ -76,16 +55,61 @@ const Product = ({ products }) => {
               </div>
 
               {/* View Details Button */}
-              <button className="hover:text-primary transition-colors">
+              <button className="hover:text-primary transition-colors"
+              onClick={() => setIsPopupOpen(true)}
+              >
                 <FaEye size={20} />
               </button>
 
             </div>
           </div>
+          
+      <DetailsButton
+      
+        part={product.part}
+        open={isPopupOpen}
+        onClose={() => setIsPopupOpen(false)}
+      />
+
         </div>
-      ))}
+      
+
+
+  )
+};
+
+const Products = ({ products }) => {
+
+  return (
+    // Main grid container
+    <div className="productbox grid 
+                xxl:text-xl
+                2xl:text-xl
+                xl:text-xl 
+                lg:text-lg 
+                slg:text-base 
+                md:text-base 
+                sm:text-sm 
+                xsm:text-sm  
+                xxsm:text-lg
+                xxl:grid-cols-5
+                2xl:grid-cols-5
+                xl:grid-cols-4
+                lg:grid-cols-4
+                slg:grid-cols-3
+                md:grid-cols-3
+                sm:grid-cols-2 
+                xsm:grid-cols-2 
+                xxsm:grid-cols-1
+                gap-6
+                p-6">
+      {products.map((product) => (
+        <>
+          <Product product={product} />
+          </>
+        ))}
     </div>
   );
 }
 
-export default Product;
+export default Products;
