21fbe407ff03c36aca07d058d310778229a73cca
שי סולומון <shay@solisti.co>
2025-02-26 02:28:31 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createRoutesPlugin } from './scripts/vite-plugin-routes';
+import { VitePWA } from 'vite-plugin-pwa';
 import { createfilesTreePlugin } from './scripts/vite-plugin-filesTree';
-import { createSupabaseConfig } from './scripts/vite-plugin-supabase-html';
+import  {createSupabaseConfig}  from './scripts/vite-plugin-supabase-html';
+
+const manifestForPlugIn = {
+  registerType: 'autoUpdate',
+  includeAssets: [],
+  manifest: {
+    name: 'kadabrix',
+    short_name: 'kadabrix',
+    description: 'kadabrix',
+    icons: [],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'any',
+  },
+  workbox: {
+    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Set limit to 5 MB
+    runtimeCaching: [
+      {
+        urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'js-css-cache',
+        },
+      },
+      {
+        urlPattern: ({ request }) => request.destination === 'document',
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'html-cache',
+        },
+      },
+    ],
+  },
+};
 
 export default defineConfig({
   build: {
-    sourcemap: true, // Enable source maps for debugging
-    minify: false, // Prevent minification for easier debugging
+
   },
   plugins: [
     react(),
-    createSupabaseConfig(),
+    createSupabaseConfig(), 
+    VitePWA(manifestForPlugIn),
     createfilesTreePlugin(),
     createRoutesPlugin({
       pagesDir: 'src/pages',
       output: 'src/routes.jsx',
     }),
+    
   ],
 });
